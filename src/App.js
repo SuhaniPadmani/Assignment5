e1edd72994ce700a0c62aca9f834ada78b6e12ef
suhani903 <suhanipadmani.it22@scet.ac.in>
2025-01-26 10:33:33 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import {BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './components/Home';
+import SignUpForm from './components/SignUpForm'
+import NoteApp from './components/NoteApp'
+
+function App() {
+  return (
+    <>
+    <Router>
+    <div>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/signup">Signup</Link></li>
+            <li><Link to="/noteapp">Note App</Link></li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='signup' element={<SignUpForm/>} />
+          <Route path='noteapp' element={<NoteApp/>}/>
+        </Routes>
+
+      </div>
+    </Router>
+    </>
+  );
+}
+
+export default App;
